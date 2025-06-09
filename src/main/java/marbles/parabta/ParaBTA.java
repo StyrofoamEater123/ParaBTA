adb548eedd551d9fb68d82d7d5f8ed8175a67eb7
StyrofoamEater123 <sputnikmarbles@proton.me>
2025-06-09 12:35:00 -0400

    Things e

--- src/main/java/marbles/parabta/ParaBTA.java
+++ src/main/java/marbles/parabta/ParaBTA.java
@@ -1,14 +1,13 @@
-package turniplabs.examplemod;
+package marbles.parabta;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+public class ParaBTA implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
     public static final String MOD_ID = "parabta";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
